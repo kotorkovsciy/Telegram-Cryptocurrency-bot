729c2b8a09df74f25402252cfa331dc4851d3831
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-06-13 01:53:22 +0300

    Switching to the DB class and setting up alerts

--- scripts/MySQL_data.py
+++ scripts/MySQL_data.py
@@ -1,35 +1,38 @@
 import sqlite3 as sq
 
-def sql_start():
-    global base, cur
-    base = sq.connect("users_time.db")
-    cur = base.cursor()
-    if base:
-        print('Data base connected OK! ')
-    base.execute('CREATE TABLE IF NOT EXISTS users(value_crypto TEXT, valutte TEXT, id_user INT)')
-    base.commit()
+class Database:
+    def __init__(self, db_file):
+        self.connection = sq.connect(db_file, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.connection.execute('CREATE TABLE IF NOT EXISTS users(value_crypto TEXT, valutte TEXT, id_user INT)')
 
-async def sql_read():
-    try: 
-        a = cur.execute(f'SELECT * FROM users').fetchmany()
-        return a
-    except:
-        print('Не найдено.')
+    async def sql_read(self):
+        with self.connection:
+            try: 
+                a = self.cursor.execute(f'SELECT * FROM users').fetchmany()
+                return a
+            except:
+                print('Не найдено.')
 
+    async def sql_quantity(self):
+        with self.connection:
+            return self.cursor.execute("SELECT count(*) FROM users").fetchone()
+            
 
-async def sql_add_command(state):
-    async with state.proxy() as data:
-        a = tuple(data.values())
-        cur.execute(f'INSERT INTO users VALUES (?, ?, ?)', (a[0], a[1], a[2]))
-        base.commit()
+    async def sql_add_command(self, state):
+        with self.connection:
+            async with state.proxy() as data:
+                a = tuple(data.values())
+                return self.cursor.executemany(f'INSERT INTO users VALUES (?, ?, ?)', (a[0], a[1], a[2]))
 
-async def sql_upName(fname, tname, lname, id_user):
-    cur.execute(f'UPDATE users SET Fname_user == ? WHERE id_user == ?', (fname, id_user,))
-    cur.execute(f'UPDATE users SET Tname_user == ? WHERE id_user == ?', (tname, id_user,))
-    cur.execute(f'UPDATE users SET Lname_user == ? WHERE id_user == ?', (lname, id_user,))
-    base.commit()
+    async def sql_upName(self, fname, tname, lname, id_user):
+        with self.connection:
+            self.cursor.executemany(f'UPDATE users SET Fname_user == ? WHERE id_user == ?', (fname, id_user,))
+            self.cursor.executemany(f'UPDATE users SET Tname_user == ? WHERE id_user == ?', (tname, id_user,))
+            self.cursor.executemany(f'UPDATE users SET Lname_user == ? WHERE id_user == ?', (lname, id_user,))
 
+    async def sql_delete(self, id_user):
+        with self.connection:
+            self.cursor.executemany(f'DELETE FROM users WHERE id_user == ?', (id_user,))
 
-async def sql_delete(id_user):
-    cur.execute(f'DELETE FROM users WHERE id_user == ?', (id_user,))
-    base.commit()
+SQL_DB = Database("users_time.db")
